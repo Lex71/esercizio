ec22fc01962d55227182645faa3dd026ee504ca7
LUCA ALBANI <41785248+Lex71@users.noreply.github.com>
2024-11-26 15:07:40 +0100

    Update BreweryService.php

--- application/app/Services/BreweryService.php
+++ application/app/Services/BreweryService.php
@@ -20,56 +20,6 @@ class BreweryService
     $this->requestUrl = join('/', [$this->baseUri, $this->path]);
   }
 
-  private function getAccessToken()
-  {
-    $client = new Client();
-    try {
-      $response = $client->post('https://login.microsoftonline.com/' . config('sharepoint.AZURE_TENANT_ID') . '/oauth2/v2.0/token', [
-        'form_params' => [
-          'client_id' => config('sharepoint.AZURE_CLIENT_ID'),
-          'client_secret' => config('sharepoint.AZURE_CLIENT_SECRET'),
-          'scope' => 'https://graph.microsoft.com/.default',
-          'grant_type' => 'client_credentials',
-        ],
-      ]);
-
-      $body = json_decode((string)$response->getBody(), true);
-      return $body['access_token'];
-    } catch (\Exception $e) {
-      Log::error('Error obtaining access token: ' . $e->getMessage());
-      throw new \Exception('Error obtaining access token: ' . $e->getMessage());
-    }
-  }
-
-  public function makeRequest($method, $requestUrl, $queryParams = [], $formParams = [], $headers = [], $hasFile = false)
-  {
-    $client = new Client([
-      'base_uri' => $this->baseUri,
-    ]);
-
-    $bodyType = 'form_params';
-
-    if ($hasFile) {
-      $bodyType = 'multipart';
-      $multipart = [];
-
-      foreach ($formParams as $name => $contents) {
-        $multipart[] = [
-          'name' => $name,
-          'contents' => $contents
-        ];
-      }
-    }
-
-    $response = $client->request($method, $requestUrl, [
-      'query' => $queryParams,
-      $bodyType => $hasFile ? $multipart : $formParams,
-      'headers' => $headers,
-    ]);
-
-    return $response->getBody()->getContents();
-  }
-
   public function getData()
   {
     $client = new Client();
